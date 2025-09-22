56e5caea4c4144fc4e83fce676a23d68a6e5c49a
zeli <zeli8888@outlook.com>
2025-09-22 16:08:40 +0100

    rest-api-lab init

---
+++ rest-api-lab/src/main/java/com/example/controller/ApiController.java
@@ -0,0 +1,40 @@
+package com.example.controller;
+
+import com.example.model.User;
+import com.example.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class ApiController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public List<User> getUsers() {
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public User getUser(@PathVariable String id) {
+        return userService.getUserById(id);
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userService.createUser(user);
+    }
+
+    @PutMapping("/{id}")
+    public User updateUser(@PathVariable String id, @RequestBody User user) {
+        return userService.updateUser(id, user);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable String id) {
+        userService.deleteUser(id);
+    }
+}
